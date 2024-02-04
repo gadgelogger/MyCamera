0ccf341581af7d210ad08a2f72e6fabce31dd5a2
gadgelogger <sarayou273142910@gmail.com>
2024-02-04 23:13:39 +0900

    add:エフェクトを追加

--- MyCamera/UIImageExtension.swift
+++ MyCamera/UIImageExtension.swift
@@ -5,4 +5,20 @@
 //  Created by gadgelogger on 2024/02/04.
 //
 
+
 import Foundation
+import UIKit
+
+extension UIImage {
+    func resize() -> UIImage? {
+        let rate = 1024.0 / self.size.width
+        let rect = CGRect(x: 0, y: 0, width: self.size.width * rate , height: self.size.height * rate)
+        
+        UIGraphicsBeginImageContext(rect.size)
+        self.draw(in: rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
